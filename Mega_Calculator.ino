ccad4ac6a21a4c2144480a77356b0a3daf94de2c
Michael P. Flaga <mflaga@tnsi.com>
2021-01-23 18:01:05 -0500

    Added support for denounce button pad

--- Mega_Calculator.ino
+++ Mega_Calculator.ino
@@ -18,6 +18,44 @@ LEDdisplay LEDdisplay(DISPLAY_SIZE);
 #include "Calculator.h"
 Calculator Calculator(DISPLAY_SIZE);
 
+#include "Debouncer.h"
+#define LENGTH_OF_ARRAY(x) ((sizeof(x)/sizeof(x[0])))
+
+typedef struct {
+  int  name;
+  char character;
+} Button;
+
+Button pins[] = {
+  // displayed in order as seen from Boards Pinout.
+  {21, '='},  
+  {20, '+'},  
+  {19, 'c'},  
+  {18, '7'},  
+  {17, '4'},  
+  {16, '1'},  
+  {15, 's'},  
+  {14, '0'},  
+//{ 0, 'R'}, // Rx of Serial0, needed for IDE.
+//{ 1, 'T'}, // Tx of Serial0
+  { 2, '2'},  
+  { 3, '5'},  
+  { 4, '8'},  
+  { 5, '('},  
+  { 6, '%'},  
+  { 7, '9'},  
+  { 8, '6'},  
+  { 9, '3'},  
+  {10, '.'}, 
+  {11, '/'},  
+  {12, '*'},  
+  {13, '-'}   
+};
+
+int debounce_duration_ms = 50;
+
+Debouncer* debounced[LENGTH_OF_ARRAY(pins)];
+
 void setup() {
   Serial.begin(115200); // enable port for debug
   Serial.println("Mega Calculator is starting");
@@ -38,6 +76,11 @@ void setup() {
   LEDdisplay.setColor(0, 255, 0);
   LEDdisplay.printDisplay("0.");
 
+  // initialize Buttons with an array of debouncers.
+  for (int position = 0; position < LENGTH_OF_ARRAY(pins); position++) {
+    pinMode(pins[position].name, INPUT_PULLUP);
+    debounced[position] = new Debouncer(pins[position].name, debounce_duration_ms, Debouncer::Active::L, Debouncer::DurationFrom::TRIGGER);
+  }
 
 } // setup() - end
 
@@ -45,7 +88,34 @@ void loop() {
 
   char displayStr[DISPLAY_SIZE + 2]; // 1 extra for Decimal and 1 extra for the str null terminator.
 
+  // get keys from debounced buttons
+  for (int position = 0; position < LENGTH_OF_ARRAY(pins); position++) {
+    debounced[position]->update();
+    if (debounced[position]->edge())
+    {
+      Serial.print("Button '");
+      Serial.print(pins[position].character);
+      Serial.print("' edge detect: ");
+      if (debounced[position]->rising())
+      {
+        Serial.println("released");
+      }
+      if (debounced[position]->falling())
+      {
+        Serial.println("pressed");
+        
+        // process button character into calculator and display
+        Calculator.parse(displayStr, pins[position].character);
+        Serial.print("displayStr["); Serial.print(strlen(displayStr)); Serial.print("]=\"");  Serial.print(displayStr); Serial.println("\"");
+        LEDdisplay.printDisplay(displayStr);
+      }
+    }
+  }
+  
+  // get keys from serial port
   while (Serial.available()) {
+    
+    // process key character into calculator and display
     Calculator.parse(displayStr, Serial.read());
     Serial.print("displayStr["); Serial.print(strlen(displayStr)); Serial.print("]=\"");  Serial.print(displayStr); Serial.println("\"");
     LEDdisplay.printDisplay(displayStr);
